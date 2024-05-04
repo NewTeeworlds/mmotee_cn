0070a7261918ad9d22785308ed4e1cfcfe8e2989
Bamcane <diandaokui@qq.com>
2024-05-04 10:57:31 +0800

    修复

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -5681,7 +5681,5 @@ void CGameContext::PrepareClientChangeMap(int ClientID)
 
 void CGameContext::SyncPlayer(int ClientID, CPlayer *pPlayer)
 {
-	if(m_apPlayers[ClientID])
-		return;
 	m_apPlayers[ClientID] = pPlayer;
 }
